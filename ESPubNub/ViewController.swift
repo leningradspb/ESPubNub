2e138a6d0fb8ba99b432d0515348905aa24fba5b
Ed <Siniakov>
2021-06-03 12:29:03 +0300

    PubNub initialized

--- ESPubNub/ViewController.swift
+++ ESPubNub/ViewController.swift
@@ -6,14 +6,53 @@
 //
 
 import UIKit
+import PubNub
 
 class ViewController: UIViewController {
+    
+    private var pubNub: PubNub {
+        var config = PubNubConfiguration(publishKey: "pub-c-fdd20a63-fd16-4a8f-930d-6cb72eb6d916", subscribeKey: "sub-c-60714650-c300-11eb-8a3a-220055b20f11")
+        config.uuid = "3dcde054-17ec-48ba-88f9-93fca230ca8a"
+        let pubNub = PubNub(configuration: config)
+        return pubNub
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        setupMessageListener()
+//        setupPubNubConfig()
     }
+    
+//    private func setupPubNubConfig() {
+//        var config = PubNubConfiguration(publishKey: "pub-c-fdd20a63-fd16-4a8f-930d-6cb72eb6d916", subscribeKey: "sub-c-60714650-c300-11eb-8a3a-220055b20f11")
+//        config.uuid = "3dcde054-17ec-48ba-88f9-93fca230ca8a"
+//        let pubNub = PubNub(configuration: config)
+//    }
+    
+    private func setupMessageListener() {
+        // Create a new listener instance
+        let listener = SubscriptionListener()
 
+        // Add listener event callbacks
+        listener.didReceiveSubscription = { event in
+          switch event {
+          case let .messageReceived(message):
+            print("Message Received: \(message) Publisher: \(message.publisher ?? "defaultUUID")")
+          case let .connectionStatusChanged(status):
+            print("Status Received: \(status)")
+          case let .presenceChanged(presence):
+            print("Presence Received: \(presence)")
+          case let .subscribeError(error):
+            print("Subscription Error \(error)")
+          default:
+            break
+          }
+        }
+
+        // Start receiving subscription events
+        pubNub.add(listener)
+    }
 
 }
 
