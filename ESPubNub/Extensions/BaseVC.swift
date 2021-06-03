46f1a57b6716f55ee03c36e48265f056602b9ae5
Ed <Siniakov>
2021-06-03 22:50:54 +0300

    delete test vc

--- ESPubNub/Extensions/BaseVC.swift
+++ ESPubNub/Extensions/BaseVC.swift
@@ -22,6 +22,7 @@ class BaseVC: UIViewController {
     }
     
     private func setupPubNubConfig() {
+//        var config = PubNubConfiguration(publishKey: "pub-c-fdd20a63-fd16-4a8f-930d-6cb72eb6d916", subscribeKey: "sub-c-60714650-c300-11eb-8a3a-220055b20f11")
         var config = PubNubConfiguration(publishKey: "pub-c-f8a06059-fb89-4b42-9e96-63f6211354a1", subscribeKey: "sub-c-3293f7fe-c44b-11eb-9292-4e51a9db8267")
         config.uuid = "3dcde054-17ec-48ba-88f9-93fca230ca8a"
         pubNub = PubNub(configuration: config)
